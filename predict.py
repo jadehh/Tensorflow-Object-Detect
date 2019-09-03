317ecf034da1a22d830413b59728d4b3d930c7d7
jade <jadehh@live.com>
2019-09-03 15:02:23 +0800

    添加图片目标检测文件

--- predict.py
+++ predict.py
@@ -7,15 +7,12 @@
 import argparse
 from jade import *
 from objects_model import ObjectModel
-import os
-import os.path as ops
 import cv2
-import tensorflow as tf
 import time
 
 paraser = argparse.ArgumentParser(description="Detect car")
 #genearl
-paraser.add_argument("--model_path",default="/home/jade/Models/HandGestureModels/ssd_mobilenet_v2_hand_gesture_2019-08-16",help="path to load model")
+paraser.add_argument("--model_path",default="/home/jade/Models/HandGestureModels/ssd_mobilenet_v1_hand_gesture_2019-08-21",help="path to load model")
 paraser.add_argument("--label_path",default="/home/jade/Data/Hand_Gesture/hand_gesture.prototxt",help="path to labels")
 paraser.add_argument("--num_classes",default=2,help="the number of classes")
 paraser.add_argument("--gpu_memory_fraction",default=0.8,help="the memory of gpu")
@@ -24,14 +21,15 @@ args = paraser.parse_args()
 
 detectionModel = ObjectModel(args)
 
-image_list = GetAllImagesPath("/home/jade/Data/FaceGesture/250_1/JPEGImages")
+image_list = GetAllImagesPath("/home/jade/Data/FaceGesture/UAFaceGesture_sample_16/JPEGImages")
 processBar = ProcessBar()
 processBar.count = len(image_list)
 
 for image_path in image_list:
     processBar.start_time = time.time()
     image = cv2.imread(image_path)
-    boxes,labels,labelIds,scores = detectionModel.predict(image,0.6)
+    # image = cv2.resize(image,(300,300))
+    boxes,labels,labelIds,scores = detectionModel.predict(image,0.9)
     CVShowBoxes(image, boxes, labels, labelIds, scores, waitkey=1000)
     #GenerateXml(GetLastDir(image_path)[:-4],image.shape,boxes,labelIds,labels,"/home/jade/Data/StaticDeepFreeze/2019-04-10_15-46-19/Annotations")
     #image = CVShowBoxes(cv2.imread(image_path),boxes,labels,labelIds,scores,waitkey=False)
