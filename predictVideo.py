b465c731dee1041e117eeb2e8af6589f62f3d2ce
jade <jadehh@live.com>
2019-09-03 15:03:53 +0800

    视频目标检测

--- predictVideo.py
+++ predictVideo.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*- 
 # 作者：Create on 2019/7/18 17:29 by jade
 # 邮箱：jadehh@live.com
-# 描述：TODO
+# 描述：视频目标检测
 # 最近修改：2019/7/18 17:29 modify by jade
 import argparse
 from jade import *
@@ -11,16 +11,16 @@ import cv2
 
 paraser = argparse.ArgumentParser(description="Detect car")
 #genearl
-paraser.add_argument("--model_path",default="/home/jade/Models/objectDetectionModels/ssd_mobilenet_v2_hand_2019-07-18/pb/ssd_mobilenet_v2_hand.pb",help="path to load model")
-paraser.add_argument("--label_path",default="/home/jade/label_map/hand.prototxt",help="path to labels")
-paraser.add_argument("--num_classes",default=1,help="the number of classes")
+paraser.add_argument("--model_path",default="/home/jade/Models/GestureFaceModels/ssd_mobilenet_v1_gesture_face_2019-08-21/",help="path to load model")
+paraser.add_argument("--label_path",default="/home/jade/Data/GestureFace/gesture_face.prototxt",help="path to labels")
+paraser.add_argument("--num_classes",default=3,help="the number of classes")
 paraser.add_argument("--gpu_memory_fraction",default=0.8,help="the memory of gpu")
 args = paraser.parse_args()
 
 
 detectionModel = ObjectModel(args)
 processBar = ProcessBar()
-videoCapture = cv2.VideoCapture("/home/jade/Data/videos/2019-07-16_17-30-52/2019-07-16_17-30-52.mp4")
+videoCapture = cv2.VideoCapture("/home/jade/Videos/IPS_2019-08-14.14.13.31.mp4")
 ret,frame = videoCapture.read()
 processBar.count = int(videoCapture.get(cv2.CAP_PROP_FRAME_COUNT))
 while ret:
